35473c407e1c501372e13a6e6b3a251cba5f26ec
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-07-09 18:48:09 -0400

    signin api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,52 @@
+const multer = require('multer');
+const jwt = require('jsonwebtoken');
 
 //sign up
 //sign in
 //crud reminders
+modules.export = function(app) {
+    const upload = multer();
+    const tokenList = {};
+
+    app.route('/signup')
+        .post(upload.none(), (req, res) => {
+            const email = req.body.email;
+            const username = req.body.username;
+            const password = req.body.password;
+            //verify email is unique and username is unique
+
+            //email or username is not unique            
+
+            //generate verfication token and send verification email
+
+        })
+    
+    app.route('/emailconfirmation/:email/:token')
+        .get((req, res) => {
+            //lookup token
+            //ensure token is still valid then validate and create user
+            //generate and send tokens and redirect
+        });
+
+    app.route('/resendemailconfirmation/:email')
+        .get((req, res) => {
+            //lookup email
+            //the email is already validated
+            //the email is not validated, send a verification email
+        });
+
+    app.route('/login')
+        .post(upload.none(), (req, res) => {
+            //user provides username and password
+            const username = req.body.username;
+            const password = req.body.password;
+
+            //verify the username and password in the database
+
+            //user does not exist
+
+            //user is verified, generate and send tokens and redirect
+
+            //invalid password
+        })
+}
