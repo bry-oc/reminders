a48befb6bd26f1755ed233a0534dbe8170231424
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-07-10 18:41:25 -0400

    password hashing and user insertion

--- server/db/auth.js
+++ server/db/auth.js
@@ -8,3 +8,7 @@ const pool = new Pool({
     database: process.env.PG_DATABASE,    
     port: process.env.PG_PORT,
 });
+
+exports.createEmailToken = async function(userID, token) {
+    return await pool.query('INSERT INTO email_token (userID, emailToken) VALUES ($1, $2) RETURNING token', [userID, token]);
+}
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const argon2 = require('argon2');
+const crypto = require('crypto');
 const multer = require('multer');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
-const usersDB = require('../db/user');
+const userQuery = require('../db/user');
+const authQuery = require('../db/auth');
 require('dotenv').config();
 
 //sign up
@@ -13,21 +16,35 @@ module.exports = function (app) {
     const upload = multer();
 
     app.route('/api/signup')
-        .post(upload.none(), (req, res) => {
+        .post(upload.none(), async (req, res) => {
             try {
                 const email = req.body.email;
                 const username = req.body.username;
                 const password = req.body.password;
                 //verify email is unique and username is unique
-                let lookup = usersDB.getUserByEmail(email);
+                let lookup = userQuery.getUserByEmail(email);
                 
                 console.log(lookup);
                 if(lookup.rowCount === 0) {
                     //email is unique
-                    let lookup = usersDB.getUserByUsername(username);
+                    let lookup = userQuery.getUserByUsername(username);
                     if(lookup.rowCount === 0) {
-                        //username is also unique
-                        //generate verfication token and send verification email
+                        //username is also unique                        
+                        //hash password
+                        const passwordHash = await argon2.hash(password);
+                        //create user that is unverified; they must become verified to use the app
+                        const userID = await userQuery.createUser(username, email, passwordHash);
+                        if (!userID) {
+                            res.status(500).end();
+                        }
+                        //userID and a 
+                        //generate verfication token
+                        const hexString = crypto.randomBytes(16).toString('hex');
+                        const emailToken = await authQuery.createEmailToken(userID, hexString);
+                        if(!emailToken) {
+                            res.status(500).end();
+                        }
+                        //send verification email
                     } else {
                         //username is being used
                         res.status(403).send('That username is already being used.').end();
@@ -43,14 +60,14 @@ module.exports = function (app) {
 
         })
 
-    app.route('/api/emailconfirmation/:email/:token')
+    app.route('/api/emailconfirmation/:hash/:token')
         .get((req, res) => {
             //lookup token
             //ensure token is still valid then validate and create user
             //generate and send tokens and redirect
         });
 
-    app.route('/api/resendemailconfirmation/:email')
+    app.route('/api/resendemailconfirmation/:hash')
         .get((req, res) => {
             //lookup email
             //the email is already validated
