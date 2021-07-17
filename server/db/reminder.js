a2f1503c59e5123318ce596eda7f54235bec085e
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-07-16 23:39:49 -0400

    added reminder queries

--- server/db/reminder.js
+++ server/db/reminder.js
@@ -13,3 +13,14 @@ const pool = new Pool({
 exports.createReminder = async function(userid, name, description, repeat, date) {
     return await pool.query('INSERT INTO "reminder" ("userid", "name", "description", "repeat", "date") VALUES ($1, $2, $3, $4, $5)', [userid, name, description, repeat, date]);
 }
+
+exports.getAllReminders = async function(userID) {
+    return await pool.query('SELECT * FROM "reminder" WHERE "userid" = $1', [userID]);
+}
+
+exports.updateReminder = async function(userID, reminderID, name, description, repeat, date) {
+    return await pool.query('UPDATE "reminder" SET "name" = $1, "description" = $2, "repeat" = $3, "date" = $4 WHERE "userid" = $5 AND "reminderid" = $6', [name, description, repeat, date, userID, reminderID]);
+}
+exports.deleteReminder = async function(userID, reminderID) {
+    return await pool.query('DELETE FROM "reminder" WHERE "userid" = $1 AND "reminderid" = $2', [userID, reminderID]);
+}
