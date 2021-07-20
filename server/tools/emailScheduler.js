55a3209568f4c82c88e6fef4ae7709ad5a742574
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-07-20 19:56:27 -0400

    finished handling for missed email reminders

--- server/tools/emailScheduler.js
+++ server/tools/emailScheduler.js
@@ -1,6 +1,7 @@
 'use strict';
 const { Pool } = require('pg');
 const reminderQuery = require('../db/reminder');
+const userQuery = require('../db/user');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
@@ -17,13 +18,22 @@ exports.checkMissedReminders = async function() {
     let currentDate = new Date().getTime();
     let lookup = await reminderQuery.checkMissedReminders(currentDate);
     let count = lookup.rowCount;
+    const reminders = lookup.rows;
     let email;
     let username;
     let reminderName;
     let reminderID;
+    let userID;
+
     //for each reminder, send an email and update db
     for(let i = 0; i < count; i++) {
-        
+        userID = reminders[i].userid;
+        lookup = await userQuery.getUserByUserID(userID);
+        email = lookup.rows[0].email;
+        username = lookup.rows[0].username;
+        reminderName = reminders[i].name;
+        reminderID = reminders[i].reminderid;
+
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -35,17 +45,25 @@ exports.checkMissedReminders = async function() {
         const mailOptions = {
             from: process.env.EMAIL_ACCOUNT,
             to: email,
-            subject: 'Account Verification Link',
-            text: 'Hello ' + username + ',\n\nThis is a reminder for the following event: ' + reminder + '\nUnfortunately, the server was experiencing technical difficulties and missed the requested reminder time.  We apologize for the inconvenience.'
+            subject: 'Reminder Notification: ' + reminderName,
+            html: 'Hello ' + username + ',<br/><br><br/><br>This is a reminder for the following event: ' + reminderName + "<br/><br>More information about this event can be found <a href='https://localhost:3000/user/" + userID + "/reminder/" + reminderID + "'>here</a>.<br/><br>Unfortunately, the server was experiencing technical difficulties and missed the requested reminder time.<br/><br>We apologize for the inconvenience."
         }
 
-        transporter.sendMail(mailOptions, function (err) {
+        
+        transporter.sendMail(mailOptions, async function (err) {
             if (err) {
                 console.log(err);
             } else {
-                console.log('Missed reminder emails have been sent.');
+                await reminderQuery.setReminderSent(reminderID);
             }
         });
     }
+
+    if(count <= 0) {
+        console.log('No reminder emails were missed.')
+    } else {
+        console.log('Missed reminder emails have been sent.');
+    }
+    
     return;
 }
