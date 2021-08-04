60523d5ef3469031eb97e14d60420a0a38b70e37
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-08-04 15:30:40 -0400

    additional update reminder testing

--- server/tests/functionalTests.js
+++ server/tests/functionalTests.js
@@ -9,48 +9,63 @@ chai.use(chaiHttp);
 
 const agent = chai.request.agent(server);
 
-suite('User login tests', function() {
-        test('user login with invalid credentials', function(done) {
-            chai.request(server)
-                .post('/api/login')
-                .type('form')
-                .send({
-                    username: 'test_name',
-                    password: 'badpassword'
-                })
-                .end(function (err, res) {
-                    assert.equal(res.status, 404);
-                    assert.equal(res.body.error, 'Login failed. Username or password did not match.');
-                    done();
-                })
-        });
-        test('user login with missing credentials', function(done) {
-            chai.request(server)
-                .post('/api/login')
-                .end(function (err, res) {
-                    assert.equal(res.status, 400);
-                    assert.equal(res.body.error, 'Missing required field(s)!');
-                    done();
-                })
-        });
-        test('user login with valid credentials', function(done) {
-            chai.request(server)
-                .post('/api/login')
-                .type('form')
-                .send({
-                    username: process.env.TEST_ACCOUNT,
-                    password: process.env.TEST_PASSWORD
-                })
-                .end(function (err, res) {
-                    assert.equal(res.status, 200);
-                    assert.equal(res.body.success, true);
-                    assert.equal(res.body.message, 'Login was successful.');
-                    done();
-                })
-        });
+suite('User login tests', function () {
+    test('user login with invalid credentials', function (done) {
+        chai.request(server)
+            .post('/api/login')
+            .type('form')
+            .send({
+                username: 'test_name',
+                password: 'badpassword'
+            })
+            .end(function (err, res) {
+                assert.equal(res.status, 404);
+                assert.equal(res.body.error, 'Login failed. Username or password did not match.');
+                done();
+            })
+    });
+    test('user login with missing credentials', function (done) {
+        chai.request(server)
+            .post('/api/login')
+            .end(function (err, res) {
+                assert.equal(res.status, 400);
+                assert.equal(res.body.error, 'Missing required field(s)!');
+                done();
+            })
+    });
+    test('user login with valid credentials', function (done) {
+        chai.request(server)
+            .post('/api/login')
+            .type('form')
+            .send({
+                username: process.env.TEST_ACCOUNT,
+                password: process.env.TEST_PASSWORD
+            })
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.success, true);
+                assert.equal(res.body.message, 'Login was successful.');
+                done();
+            })
+    });
 });
 
-suite('Create reminders tests', function () {    
+suite('Create reminders tests', function () {
+    test('create reminder without authorization', function (done) {
+        chai.request(server)
+            .post('/api/reminder/create')
+            .type('form')
+            .send({
+                reminderName: "test_reminder",
+                reminderDate: "01/02/2022",
+                reminderTime: "11:30",
+                timezone: 0
+            })
+            .end(function (err, res) {
+                assert.equal(res.status, 401);
+                done();
+            })
+    })
     test('create reminder with missing required fields', function (done) {
         agent.post('/api/login')
             .type('form')
@@ -58,14 +73,14 @@ suite('Create reminders tests', function () {
                 username: process.env.TEST_ACCOUNT,
                 password: process.env.TEST_PASSWORD
             })
-            .end(function(req, res) {
+            .end(function (req, res) {
                 agent.post('/api/reminder/create')
-                    .end(function(err, res){
-                        assert.equal(res.status, 400);      
-                        assert.equal(res.body.error, 'Missing required field(s)!');          
+                    .end(function (err, res) {
+                        assert.equal(res.status, 400);
+                        assert.equal(res.body.error, 'Missing required field(s)!');
                         done();
-                    })    
-            })        
+                    })
+            })
     });
     test('create reminder with valid fields', function (done) {
         agent.post('/api/login')
@@ -139,8 +154,78 @@ suite('Create reminders tests', function () {
     });
 });
 
-suite('Update reminder tests', function() {
-    test('Update reminder with valid fields', function(done) {
+suite('Update reminder tests', function () {
+    test('Update reminder without authentication', function(done) {
+        agent.post('/api/login')
+            .type('form')
+            .send({
+                username: process.env.TEST_ACCOUNT,
+                password: process.env.TEST_PASSWORD
+            })
+            .end(function (req, res) {
+                agent.post('/api/reminder/create')
+                    .type('form')
+                    .send({
+                        reminderName: "test_reminder",
+                        reminderDate: "01/02/2022",
+                        reminderTime: "02:30",
+                        timezone: 0
+                    })
+                    .end(function (err, res) {
+                        assert.equal(res.status, 200);
+                        assert.equal(res.body.success, true);
+                        assert.exists(res.body.reminderid);
+                        const reminderid = res.body.reminderid;
+                        chai.request(server)
+                            .post('/api/reminder/update')
+                            .type('form')
+                            .send({
+                                reminderid: reminderid,
+                                reminderName: "updated_test_reminder",
+                                reminderDate: "01/03/2022",
+                                reminderTime: "05:00",
+                                reminderDescription: "new_description",
+                                reminderRepeat: "never",
+                                timezone: 0
+                            })
+                            .end(function (err, res) {
+                                assert.equal(res.status, 401);
+                                done();
+                            })
+                    })
+            })
+    });
+    test('Update reminder with missing fields', function (done) {
+        agent.post('/api/login')
+            .type('form')
+            .send({
+                username: process.env.TEST_ACCOUNT,
+                password: process.env.TEST_PASSWORD
+            })
+            .end(function (req, res) {
+                agent.post('/api/reminder/create')
+                    .type('form')
+                    .send({
+                        reminderName: "test_reminder",
+                        reminderDate: "01/02/2022",
+                        reminderTime: "02:30",
+                        timezone: 0
+                    })
+                    .end(function (err, res) {
+                        assert.equal(res.status, 200);
+                        assert.equal(res.body.success, true);
+                        assert.exists(res.body.reminderid);
+                        const reminderid = res.body.reminderid;
+                        agent.post('/api/reminder/update')
+                            .end(function (err, res) {
+                                assert.equal(res.status, 400);
+                                assert.equal(res.body.error, 'Missing required field(s)!');
+                                done();
+                            })
+                    })
+            })
+    })
+    test('Update reminder with valid fields', function (done) {
         agent.post('/api/login')
             .type('form')
             .send({
