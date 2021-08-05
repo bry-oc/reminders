897c5d737f9291ffe05d15818c61f060a3c647e6
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-08-05 14:01:40 -0400

    validate dates testing

--- server/tests/unitTests.js
+++ server/tests/unitTests.js
@@ -42,12 +42,38 @@ suite('Unit tests', function() {
         test('Validation test for invalid passwords', function () {
             assert.equal(validator.isValidPassword('a1234567'), false, 'a1234567');
             assert.equal(validator.isValidPassword('example1231212'), false, 'example1231212');
-            assert.equal(validator.isValidUsername(''), false, 'empty');
-            assert.equal(validator.isValidUsername(' '), false, 'space');
+            assert.equal(validator.isValidPassword(''), false, 'empty');
+            assert.equal(validator.isValidPassword(' '), false, 'space');
             assert.equal(validator.isValidPassword('a12B34567'), false, 'a12B34567');
             assert.equal(validator.isValidPassword('aB!@#$#$#$#$%'), false, 'aB!@#$#$#$#$%');
             assert.equal(validator.isValidUsername('df yvpoyroiyqwuryvvwyeorviyqwoieryoweqiry'), false, 'df yvpoyroiyqwuryvvwyeorviyqwoieryoweqir');
         });
-        
+        test('Validation test for valid dates', function () {
+            assert.equal(validator.isValidDate('01/01/1900'), true, '01/01/1900');
+            assert.equal(validator.isValidDate('01/02/0000'), true, '01/02/0000');
+            assert.equal(validator.isValidDate('02/29/1940'), true, '02/29/1940');
+            assert.equal(validator.isValidDate('11/11/2000'), true, '11/11/2000');
+            assert.equal(validator.isValidDate('12/22/2020'), true, '12/22/2020');
+            assert.equal(validator.isValidDate('02/01/1335'), true, '02/01/1335');
+            assert.equal(validator.isValidDate('03/10/1995'), true, '03/10/1995');
+            assert.equal(validator.isValidDate('04/30/1450'), true, '04/30/1450');
+            assert.equal(validator.isValidDate('05/31/1777'), true, '05/31/1777');
+            assert.equal(validator.isValidDate('06/17/1811'), true, '06/17/1811');
+            assert.equal(validator.isValidDate('07/22/1269'), true, '07/22/1269');
+            assert.equal(validator.isValidDate('08/15/1780'), true, '08/15/1780');
+            assert.equal(validator.isValidDate('09/24/1902'), true, '09/24/1902');
+            assert.equal(validator.isValidDate('10/18/1667'), true, '10/18/1667');
+        });
+        test('Validation test for invalid dates', function () {
+            assert.equal(validator.isValidDate(''), false, 'empty');
+            assert.equal(validator.isValidDate('au'), false, 'au');
+            assert.equal(validator.isValidDate('abcde'), false, 'abcde');
+            assert.equal(validator.isValidDate('99/01/2022'), false, '99/01/2022');
+            assert.equal(validator.isValidDate('01/99/2022'), false, '01/99/2022');
+            assert.equal(validator.isValidDate('01/01'), false, '01/01');
+            assert.equal(validator.isValidDate('99/99/9999'), false, '99/99/9999');
+            assert.equal(validator.isValidDate('111/11/2000'), false, '111/11/2000');
+            assert.equal(validator.isValidDate('11/1221/2000'), false, '111/1221/2000');
+        });
     })    
 })
