b431cf4d4c44b9a67c268c8a457573b56ae85b96
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-08-10 15:57:49 -0400

    account html

---
+++ client/src/components/Account.js
@@ -0,0 +1,9 @@
+function Account(){
+    return(
+        <div>
+            <button>Change Username</button>
+            <button>Change Email</button>
+            <button>Change Password</button>
+        </div>
+    )
+}
