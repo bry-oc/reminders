347dce70e96e94f52bd4229d713e8fd9f1fc09f9
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-08-11 15:54:31 -0400

    home css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ function Home(){
     return(
         <div className="wrapper">
             <div className="title">
-                <h1>Welcome</h1>
+                <h1>Reminder App: Never Forget</h1>
             </div>
             <div className="reminder-image">
                 <img src="https://images.unsplash.com/photo-1435527173128-983b87201f4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1647&q=80" alt="an open planner"></img>
