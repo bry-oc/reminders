ba3b0dd174f518d369539edc0f29ae545d6404e3
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-08-12 15:37:29 -0400

    create reminder fetch

--- client/src/components/CreateReminder.js
+++ client/src/components/CreateReminder.js
@@ -1,32 +1,66 @@
 function CreateReminder(){
+    let createReminder = (e) => {
+        e.preventDefault();
+        const reminderName = e.target.name.value;
+        const reminderDate = e.target.date.value;
+        const reminderTime = e.target.time.value;
+        const reminderRepeat  = e.target.repeat.value;
+        const reminderDescription = e.target.description.value;
+
+        const formData = new FormData();
+        formData.append('reminderName', reminderName);
+        formData.append('reminderDate', reminderDate);
+        formData.append('reminderTime', reminderTime);
+        formData.append('reminderRepeat', reminderRepeat);
+        formData.append('reminderDescription', reminderDescription);
+        console.log(formData);
+
+        const url = '/api/reminder/create'
+
+        fetch(url, { 
+            method: 'POST',
+            body: formData,
+            credentials: 'include'
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.error) {
+                    console.log(data.error);
+                } else {
+                    console.log(data);
+                    window.location.href = "/account";
+                }
+            })
+        
+    }
     return (
         <div className="wrapper">
-            <form>
-                <label for="name">Name:<br />
-                    <input type="text" placeholder="Enter reminder name" required>
+            <form onSubmit={createReminder}>
+                <label htmlFor="name">Name:<br />
+                    <input type="text" placeholder="Enter reminder name" id="name" required>
+                    </input>
+                </label><br />                
+                <label htmlFor="date">Date:<br />
+                    <input type="date" id="date" required>
                     </input>
                 </label><br />
-                <label for="description">Description (Optional):<br />
-                    <textarea name="description" placeholder="Enter your description here...">
-                    </textarea>
-                </label><br />
-                <label for="date">Date:<br />
-                    <input type="date">
+                <label htmlFor="time">Time:<br />
+                    <input type="time" id="time" required>
                     </input>
                 </label><br />
-                <label for="time">Time:<br />
-                    <input type="time">
-                    </input>
-                </label><br />
-                <label for="repeat">Repeat:<br />
-                    <select form="repeat">
-                        <option value="none" selected>None</option>
-                        <option value="daily">Daily</option>
-                        <option value="weekly">Weekly</option>
-                        <option value="biweekly">Biweekly</option>
-                        <option value="monthly">Monthly</option>
+                <label htmlFor="repeat">Repeat:<br />
+                    <select id="repeat">
+                        <option value="none" id="none" selected>None</option>
+                        <option value="daily" id="daily">Daily</option>
+                        <option value="weekly" id="weekly">Weekly</option>
+                        <option value="biweekly" id="biweekly">Biweekly</option>
+                        <option value="monthly" id="monthly">Monthly</option>
                     </select>
                 </label><br />
+                <label htmlFor="description">Description (Optional):<br />
+                    <textarea name="description" placeholder="Enter your description here..." id="description">
+                    </textarea>
+                </label><br />                
                 <button type="submit">Create Reminder</button>
             </form>
         </div>
