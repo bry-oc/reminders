95577841b4f945891bcbdf67a7a7bd251b17010e
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-08-19 16:54:41 -0400

    forgotPassword fetch

--- client/src/components/ForgotPassword.js
+++ client/src/components/ForgotPassword.js
@@ -1,7 +1,29 @@
 function ForgotPassword() {
+    let forgotPassword = (e) => {
+        const url = '/api/user/password/reset';
+
+        const email = e.target.email.value;
+
+        const formData = new FormData();
+        formData.append('email', email);
+
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+            credentials: 'include'
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.error) {
+                    console.log(data.error);
+                } else {
+                    console.log(data.message);
+                }
+            })
+    }
     return (
         <div className="wrapper">
-            <form>
+            <form onSubmit={forgotPassword}>
                 <label>Email:<br />
                     <input type="text" placeholder="Enter your email" required>
                     </input>
