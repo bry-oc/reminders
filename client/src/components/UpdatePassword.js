176c5e7911ddab70ecfe1447e0c46e219aec3711
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-08-19 16:41:05 -0400

    update password fetch

--- client/src/components/UpdatePassword.js
+++ client/src/components/UpdatePassword.js
@@ -1,7 +1,30 @@
 function UpdatePassword() {
+    let updatePassword = (e) => {
+        e.preventDefault();
+        const password = e.target.password.value;
+
+        const formData = new FormData();
+        formData.append('password', password);
+
+        const url = '/api/user/password/update'
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+            credentials: 'include'
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.error) {
+                    console.log(data.error);
+                } else {
+                    console.log(data.message);
+                }
+            })
+    }
+
     return (
         <div className="wrapper">
-            <form>
+            <form onSubmit={updatePassword}>
                 <label for="password">New Password:<br />
                     <input type="text" placeholder="Enter your password" required>
                     </input>
