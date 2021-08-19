ae10ba74787a7cfa3ebf73977a8c780b46da293e
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-08-19 16:52:20 -0400

    signup fetch

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,7 +1,33 @@
 function Signup() {
+    let signup = (e) => {
+        const url = '/api/signup';
+
+        const email = e.target.email.value;
+        const username = e.target.username.value;
+        const password = e.target.password.value;
+
+        const formData = new FormData();
+        formData.append('email', email);
+        formData.append('username', username);
+        formData.append('password', password);
+
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+            credentials: 'include'
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.error) {
+                    console.log(data.error);
+                } else {
+                    console.log(data.message);
+                }
+            })
+    }
     return (
         <div className="wrapper">
-            <form>
+            <form onSubmit={signup}>
                 <label>Email:<br />
                     <input type="text" placeholder="Enter your email" required>
                     </input>
