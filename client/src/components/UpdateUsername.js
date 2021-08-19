45b3715c50cae76c0514cc2bd2ef034a67282f54
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-08-19 16:42:02 -0400

    update username fetch

--- client/src/components/UpdateUsername.js
+++ client/src/components/UpdateUsername.js
@@ -1,7 +1,29 @@
 function UpdateUsername(){
+    let updateUsername = (e) => {
+        e.preventDefault();
+        const username = e.target.username.value;
+
+        const formData = new FormData();
+        formData.append('username', username);
+
+        const url = '/api/user/username/update'
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+            credentials: 'include'
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.error) {
+                    console.log(data.error);
+                } else {
+                    console.log(data.message);
+                }
+            })
+    }
     return (
         <div className="wrapper">
-            <form>
+            <form onSubmit={updateUsername}>
                 <label for="username">New Username:<br />
                     <input type="text" placeholder="Enter your username" required>
                     </input>
