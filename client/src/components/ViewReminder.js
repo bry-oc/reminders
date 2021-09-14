336e507e37a79139c0e22f0a2ae7fc978e44fab9
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-09-14 18:20:22 -0400

    bugfixing

--- client/src/components/ViewReminder.js
+++ client/src/components/ViewReminder.js
@@ -11,7 +11,7 @@ function ViewReminder(props) {
     const [reminderRepeat, setReminderRepeat] = useState('');
     const [loading, setLoading] = useState(true);
 
-
+    //gets stuck loading if reopening same reminder
     useEffect(() => {
         setLoading(true);
         setReminderID(props.reminderid);        
