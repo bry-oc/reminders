9304047dede30fc9b9285a931c82d2ca62b02ffd
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-09-15 11:19:13 -0400

    edit modal loading properly

--- client/src/components/ListReminders.js
+++ client/src/components/ListReminders.js
@@ -166,7 +166,73 @@ function ListReminders() {
         modalEdit.style.display = "flex";
         document.body.style.overflow = "hidden";
         setCurrentID(e.target.name);
-    }    
+
+        setReminderID(e.target.name);
+        let fetchID = e.target.name;
+        const url = '/api/user/authentication';
+
+        fetch(url, {
+            method: 'GET',
+            credentials: 'include'
+        })
+            .then((res) => {
+                if (res.status === 200) {
+                    setAuth(true);
+                    fetchGetReminder(fetchID);
+                    console.log(auth);
+                } else {
+                    console.log('refresh!');
+                    const refresh = '/api/token/refresh';
+                    fetch(refresh, {
+                        method: 'GET',
+                        credentials: 'include'
+                    })
+                        .then((res) => {
+                            if (res.status === 200) {
+                                setAuth(true);
+                                fetchGetReminder(fetchID);
+                            } else {
+                                console.log(res.status);
+                                setAuth(false);
+                            }
+                        })
+                }
+            })
+    }
+
+
+    function fetchUpdateReminder() {
+        const updateReminderURL = '/api/reminder/update';
+        fetch(updateReminderURL, {
+            method: 'POST',
+            credentials: 'include'
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.error) {
+                    console.log(data.error);
+                } else {
+                    setReminderID(data.reminderid);
+                    setReminderName(data.reminderName);
+                    setReminderDescription(data.reminderDescription);
+                    setReminderDate(data.reminderDate);
+                    setReminderTime(data.reminderTime);
+                    setReminderRepeat(data.reminderRepeat);
+                }
+            })
+    }
+
+    let closeEdit = (e) => {
+        let modalEditClose = document.getElementById("modal-update");
+        modalEditClose.style.display = "none";
+        document.body.style.overflow = "auto";
+        setReminderDescription('');
+        setReminderName('');
+        setReminderDate('');
+        setReminderTime('');
+        setReminderRepeat('');
+        setLoading(true);
+    }
 
     let openDelete = (e) => {
         let modalDelete = document.getElementById("modal-delete");
@@ -215,36 +281,79 @@ function ListReminders() {
             </div>
             <div className="modal-wrapper" id="modal-view">
                 {loading ? (
-                <div className="wrapper modal">
-                    <i className="fa fa-times-circle fa-2x" onClick={closeView}></i><br />
-                    <h2>View Reminder</h2>
-                    <i className="fa fa-spinner fa-pulse fa-2x" id="spinner"></i>
-                </div> ):
-                (
-                <div className="wrapper modal">
-                    <i className="fa fa-times-circle fa-2x" onClick={closeView}></i><br />
-                    <h2>View Reminder</h2>
-                    <h3>Name: </h3>
-                    <p>{reminderName}</p>
-                    <h3>Date: </h3>
-                    <p>{reminderDate}</p>
-                    <h3>Time: </h3>
-                    <p>{reminderTime}</p>
-                    <h3>Repeat: </h3>
-                    <p>{reminderRepeat}</p>
-                    <h3>Description: </h3>
-                    <p>{reminderDescription}</p>
-                </div>
-                )}
+                    <div className="wrapper modal">
+                        <i className="fa fa-times-circle fa-2x" onClick={closeView}></i><br />
+                        <h2>View Reminder</h2>
+                        <i className="fa fa-spinner fa-pulse fa-2x" id="spinner"></i>
+                    </div>
+                    ) 
+                    :
+                    (
+                        <div className="wrapper modal">
+                            <i className="fa fa-times-circle fa-2x" onClick={closeView}></i><br />
+                            <h2>View Reminder</h2>
+                            <h3>Name: </h3>
+                            <p>{reminderName}</p>
+                            <h3>Date: </h3>
+                            <p>{reminderDate}</p>
+                            <h3>Time: </h3>
+                            <p>{reminderTime}</p>
+                            <h3>Repeat: </h3>
+                            <p>{reminderRepeat}</p>
+                            <h3>Description: </h3>
+                            <p>{reminderDescription}</p>
+                        </div>
+                    )}
             </div>
             <div className="modal-wrapper" id="modal-update">
-                <UpdateReminder reminderid={currentID} />
-            </div>
+                {loading ? (
+                    <div className="wrapper modal">
+                        <i className="fa fa-times-circle fa-2x" onClick={closeView}></i><br />
+                        <h2>View Reminder</h2>
+                        <i className="fa fa-spinner fa-pulse fa-2x" id="spinner"></i>
+                    </div>
+                )
+                :
+                (
+                    <div className="wrapper modal">
+                        <i className="fa fa-times-circle fa-2x" onClick={closeEdit}></i><br />
+                        <h2>Edit Reminder</h2>
+                        <form>
+                            <label htmlFor="name" id="name">Name:<br />
+                                <input type="text" value={reminderName} onChange={(e => setReminderName(e.target.value))} required>
+                                </input>
+                            </label><br /><br />
+                            <label htmlFor="date">Date:<br />
+                                <input type="date" value={reminderDate} onChange={(e => setReminderDate(e.target.value))}>
+                                </input>
+                            </label><br /><br />
+                            <label htmlFor="time">Time:<br />
+                                <input type="time" value={reminderTime} onChange={(e => setReminderTime(e.target.value))}>
+                                </input>
+                            </label><br /><br />
+                            <label htmlFor="repeat">Repeat:<br />
+                                <select form="repeat" value={reminderRepeat} onChange={(e => setReminderRepeat(e.target.value))}>
+                                    <option defaultValue="none" id="none">None</option>
+                                    <option value="daily" id="daily">Daily</option>
+                                    <option value="weekly" id="weekly">Weekly</option>
+                                    <option value="biweekly" id="biweekly">Biweekly</option>
+                                    <option value="monthly" id="monthly">Monthly</option>
+                                </select>
+                            </label><br /><br />
+                            <label htmlFor="description">Description (Optional):<br />
+                                <textarea name="description" value={reminderDescription} onChange={(e => setReminderDescription(e.target.value))}>
+                                </textarea>
+                            </label><br /><br />
+                            <button type="submit" onClick={fetchUpdateReminder}>Update Reminder</button>
+                        </form>
+                    </div>
+                )}
+            </div >
             <div className="modal-wrapper" id="modal-delete">
                 <DeleteReminder reminderid={currentID} />
             </div>
-        </div>
-        
+        </div >
+
     )
 
 
